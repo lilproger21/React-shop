6fe889c1dd18446533571f34d0644fbfdc0f81ff
lilproger21 <nnikolastihomirov@gmail.com>
2022-07-27 18:24:42 +0300

    first commit

---
+++ src/components/Product.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react"
+import { IProduct } from "../models"
+
+interface ProductProps {
+    product: IProduct
+}
+
+export function Product(props: ProductProps) {
+    const [details, setDetails] = useState(false);
+
+    const btnBgClassName = details ? 'bg-yellow-400' : 'bg-blue-400';
+
+    const btnClasses = ['py-2 px-4 border bg-yellow-400', btnBgClassName]
+
+    return (
+        <div
+            className="border py-2 px-4 rounded flex flex-col items-center mb-2"
+        >
+            <img src={props.product.image} className="w-1/6" alt={props.product.title} />
+            <p>{props.product.title}</p>
+            <p className="font-bold">{props.product.price}</p>
+            <button 
+            className={btnClasses.join(' ')}
+            onClick={() => setDetails( prev => !prev)}
+            >
+                { details ? 'Hide details' : 'Show details'}
+            </button>
+            {details && <div>
+                <p>{props.product.description}</p>
+                <p>Rate: <span style={{ fontWeight: 'bold'}}>{props.product?.rating?.rate}</span></p>
+            </div>}
+        </div>
+    )
+}
---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,5 @@
+export function AboutPage() {
+    return (
+        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum dicta, tempore fugiat fuga libero voluptatem ea sit itaque molestiae velit explicabo necessitatibus minus! Iste, inventore magnam repellat aperiam at quasi.</div>
+    )
+}
